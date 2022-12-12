2934328746dc8bdf62c1d6ff739ec9393e6412e4
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-12-12 18:03:56 +0100

    Added DBContext

---
+++ national-football-matchesAPI/Data/APIContext.cs
@@ -0,0 +1,15 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+
+namespace national_football_matchesAPI.Data
+{
+	public class APIContext : DbContext
+	{
+		public APIContext(DbContextOptions<APIContext> options) : base(options)
+		{
+
+		}
+
+	}
+}
