ad79df0e64701165c18b49a63ea37408428810c1
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-12-12 17:59:17 +0100

    Match Model

---
+++ national-football-matchesAPI/Models/Match.cs
@@ -0,0 +1,23 @@
+﻿namespace national_football_matchesAPI.Models
+{
+	public class Match
+	{
+		public DateOnly date { get; set; }
+
+		public string home_team { get; set; }
+
+		public string away_team { get; set; }
+
+		public int home_score { get; set; }
+
+		public int away_score { get; set; }
+
+		public string tournament { get; set; }
+
+		public string city { get; set; }
+
+		public string country { get; set; }
+
+
+	}
+}
